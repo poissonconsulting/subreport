8d93e494ee50afc2b4c74a644998abe6d212f6a3
Joe Thorley <joe@poissonconsulting.ca>
2019-01-22 10:24:50 -0800

    get knit_report working

--- R/knit.R
+++ R/knit.R
@@ -1,41 +1,36 @@
 #' Knit Report
 #' 
-#' Knit's tables, code blocks and plots (and windows) into a html file and 
-#' if browse = TRUE opens in the default web browser.
+#' Makes a results .Rmd file of the tables, code blocks and plots (and windows)
+#' and knits it into a .html file and opens
+#' in the default web browser.
 #'
-#' @param file A string of the output file name without an extension.
-#' @param report A string of the directory to save the output file.
+#' @param file A string of the file name(s) without an extension.
+#' @param report A string of the directory to save the tables, code blocks and plots.
 #' @inheritParams subfoldr2::sbf_save_object
 #' @inheritParams rmarkdown::render
-#' @param browse A flag specifying whether to open the html file.
-#' @return An invisible path to the .html file.
+#' @param browse A flag specifying whether to open the .html file in a web browser.
+#' @return An invisible path to the .Rmd file.
 #' @export
 #'
 #' @examples
-sbr_knit_report <- function(file = "report", 
+sbr_knit_report <- function(file = "results", 
                             report = sbr_get_report(),
                             sub = subfoldr2::sbf_get_sub(),
                             main = subfoldr2::sbf_get_main(),
-                            quiet = FALSE, browse = TRUE) {
+                            quiet = FALSE,
+                            browse = TRUE) {
   check_string(file)
   check_string(report)
   check_vector(sub, "", length = c(0L, 1L))
   check_string(main)
-  check_flag(browse)
-  
-  report <- sanitize_path(report, rm_leading = FALSE)
-  sub <- sanitize_path(sub)
-  main <- sanitize_path(main, rm_leading = FALSE)
-  
-  input <- file_path(report, file)
-  input <- replace_ext(input, "Rmd")
 
-  dir.create(dirname(input), showWarnings = FALSE, recursive = TRUE)
-
+  dir.create(dirname(file), showWarnings = FALSE, recursive = TRUE)
+  input <- paste0(file, ".Rmd")
   writeLines(report_text(sub = sub, main = main), con = input)
 
-  path <- rmarkdown::render(input = input, output_file = file)
+  path <- rmarkdown::render(input = input, output_format = "html_document",
+                            quiet = quiet)
   if(isTRUE(browse))
     utils::browseURL(paste0('file://', path))
-  invisible(path)
+  invisible(input)
 }
--- tests/testthat/test-knit.R
+++ tests/testthat/test-knit.R
@@ -1,18 +1,20 @@
 context("knit")
 
 test_that("tables", {
-  teardown(sbr_reset_report())
 
-  sbr_set_report(tempdir(), "report", rm = TRUE, ask = FALSE)
- 
-  sbr_set_report("report", rm = TRUE, ask = FALSE)
-   
+  dir <- file.path(tempdir(), "knit")
+  unlink(dir)
+
+  teardown(subfoldr2::sbf_reset_main())
+  subfoldr2::sbf_set_main(tempdir(), "output", rm = TRUE, ask = FALSE)
+
   x <- data.frame(obs = "JD", count = 1L)
-  subfoldr2::sbf_get_main()
-  subfoldr2::sbf_get_sub()
-
   subfoldr2::sbf_save_table(x, caption = "Observations")
   
+  file <- file.path(dir, "res")
   
-  sbr_knit_report()
+  expect_identical(sbr_knit_report(file, browse = FALSE, quiet = TRUE),
+                   paste0(file, ".Rmd"))
+  
+  expect_identical(sort(list.files(dir)), sort(c("report", "res.html", "res.Rmd")))
 })
