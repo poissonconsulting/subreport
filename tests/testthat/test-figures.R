109b4cff3db6dfbeeddc1b6ffdfc6c52f195635a
Joe Thorley <joe@poissonconsulting.ca>
2019-01-23 10:44:32 -0800

    more sub sort tests

--- tests/testthat/test-figures.R
+++ tests/testthat/test-figures.R
@@ -37,3 +37,28 @@ test_that("figures", {
   txt <- sbr_figures()
   expect_match(txt, "\n<figure>\n<img alt = \".*plots/x.png\" width = \"200%\">\n<figcaption>Figure 2. the window one.</figcaption>\n</figure>\n")
 })
+
+test_that("figures sort sub and name", {
+  teardown(subfoldr2::sbf_reset_main())
+  teardown(sbr_reset_report())
+
+  subfoldr2::sbf_set_main(tempdir(), "output", rm = TRUE, ask = FALSE)
+  sbr_set_report(tempdir(), "report", rm = TRUE, ask = FALSE)
+  
+  data <- data.frame(x = 1, y = 2)
+  allometry <- ggplot2::ggplot(data = data, ggplot2::aes(x = x, y = y))
+  subfoldr2::sbf_save_plot(allometry, caption = "Length weight")
+
+  data <- data.frame(x = 1:2, y = 3:4)
+  standard <- ggplot2::ggplot(data = data, ggplot2::aes(x = x, y = y))
+  
+  subfoldr2::sbf_save_plot(standard, sub = "> 90 mm")
+  
+  txt <- sbr_figures()
+  expect_match(txt, "\n<figure>\n<img alt = .* width = \"100%\">\n<figcaption>Figure 1. Length weight.</figcaption>\n</figure>\n\n#### > 90 Mm\n\n<figure>\n<img alt = .*report/plots/> 90 mm/standard.png\" width = \"100%\">\n<figcaption>Figure 2.</figcaption>\n</figure>\n")
+  
+  txt <- sbr_figures(sort = c("allometry", "> 90 mm", "standard"))
+
+  expect_match(txt, "\n#### > 90 Mm\n\n<figure>\n<img alt = .*report/plots/> 90 mm/standard.png\" width = \"100%\">\n<figcaption>Figure 1.</figcaption>\n</figure>\n\n<figure>\n<img alt = .*plots//allometry.png\" width = \"100%\">\n<figcaption>Figure 2. Length weight.</figcaption>\n</figure>\n")
+})
+
