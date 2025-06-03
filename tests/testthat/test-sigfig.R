395296772a33272e701a8c29da6ec0243faf14b0
Seb Dalgarno <seb@poissonconsulting.ca>
2025-06-03 13:13:30 -0700

    add case of all missing

--- tests/testthat/test-sigfig.R
+++ tests/testthat/test-sigfig.R
@@ -6,7 +6,8 @@ test_that("sigfig function works correctly on various numeric types", {
     scientific = c(1.23e-5, 4.56e8, 7.89e-10, 2.34e12),
     integer = c(123L, 456L, 789L, 0L),
     character = c("text", "more", "data", "test"),
-    logical = c(TRUE, FALSE, TRUE, FALSE)
+    logical = c(TRUE, FALSE, TRUE, FALSE),
+    missing = c(NA_real_, NA_real_, NA_real_, NA_real_)
   )
   
   res <- signif_table(df)
@@ -15,6 +16,7 @@ test_that("sigfig function works correctly on various numeric types", {
   expect_equal(res$large_number, c(1230000, 9880000, 5560, 1000000))
   expect_equal(res$scientific, c(1.23e-5, 4.56e8, 7.89e-10, 2.34e12))
   expect_equal(res$integer, c(123, 456, 789, 0))
+  expect_equal(res$missing, c(NA_real_, NA_real_, NA_real_, NA_real_))
   
   expect_identical(res$character, df$character)
   expect_identical(res$logical, df$logical)
