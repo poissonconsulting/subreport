374efaa562d57d73ae6b3c92de46780a6425b90f
Joe Thorley <joe@poissonconsulting.ca>
2026-01-21 10:44:15 -0800

    Add function to knit report Fixes #30

--- R/use.R
+++ R/use.R
@@ -1,11 +1,11 @@
-#' Use Project Rmd File
+#' Use Report File
 #'
-#' Creates a project.Rmd file in the root directory.
+#' Creates a report.Rmd file in the root directory.
 #' 
 #' @export
-sbr_use_project_rmd <- function() {
+sbr_use_report <- function() {
   usethis::use_template(
-    template = "project.Rmd",
+    template = "report.Rmd",
     package = "subreport"
   )
 }
