df1c1643dadfff2fa57dd07e28db2b6d417e6178
Joe Thorley <joe@poissonconsulting.ca>
2026-01-21 12:56:43 -0800

    Add comment

--- R/subreport-package.R
+++ R/subreport-package.R
@@ -2,6 +2,7 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+## Import term as_term as package required for converting term vectors to character vectors.
 #' @importFrom term as_term
 ## usethis namespace: end
 
